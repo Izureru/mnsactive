8b9eea0e530bc54a2a52a386315c1d4d4181134f
Israel <israel.lewis@mnscorp.net>
2015-06-12 19:46:14 +0100

    temboo trial

---
+++ MNSACTIVE/MNSACTIVE-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MNSACTIVE-Bridging-Header.h
+//  MNSACTIVE
+//
+//  Created by Izureru on 12/06/2015.
+//  Copyright (c) 2015 Marks and Spencer. All rights reserved.
+//
+
+#ifndef MNSACTIVE_MNSACTIVE_Bridging_Header_h
+#define MNSACTIVE_MNSACTIVE_Bridging_Header_h
+
+#import <Temboo/core/TMBFitbit.h>
+#endif
